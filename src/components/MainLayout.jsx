87f55706b40ba9913746527f9a66bd84886ebdb1
MokhtarMahmoud <mokhtarmm17@gmail.com>
2025-10-06 13:46:04 +0300

    hideSecandHeader

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import Sidebar from "./Sidebar";
 import { Box } from "@mui/material";
 import { getUserCookie } from "../hooks/authCookies";
+import { useLocation } from "react-router-dom";
 
 const MainLayout = ({ children }) => {
-
+ const location = useLocation();
   const user = getUserCookie();
   const isAuthenticated = Boolean(user); 
+  const hideSecandHeader = location.pathname == "/home";
 
   return (
     <Box sx={{ display: "flex", minHeight: "100vh" }}>
-     {isAuthenticated && <Sidebar />}
+     {isAuthenticated && !hideSecandHeader &&<Sidebar />}
       <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
         {/* Header لو عندك */}
         <Box component="main" sx={{ flex: 1 }}>
