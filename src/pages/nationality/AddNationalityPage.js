9a4602f28ef789748efdf018faddc69899a1ef7b
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2025-11-10 12:52:42 +0200

    ayman 10/11/2025

--- src/pages/nationality/AddNationalityPage.js
+++ src/pages/nationality/AddNationalityPage.js
@@ -130,7 +130,7 @@ export default function AddNationalityPage() {
         notify(t("success"),"success");
 
         navigate('/nationality');
-        
+
       } catch (error) {
         console.error("Error logging in:", error);
          notify(t("error"), "error");
