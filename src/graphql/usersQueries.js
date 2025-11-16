824896467968bcb57bd432c883f7e6d85ec60497
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2025-11-16 14:42:25 +0200

    ayman 16/11/2025

--- src/graphql/usersQueries.js
+++ src/graphql/usersQueries.js
@@ -253,38 +253,4 @@ query GetUserStudyMaterialsByUser($user_id:ID!) {
 }
 `
 
-// export const GET_USER_STUDY_MATERIALS_BY_USER_ID=gql`
-// query GetUserStudyMaterialsByUser($user_id : ID!) {
-//     getUserStudyMaterialsByUser(user_id: $user_id) {
-//         id
-//         status
-//         createdAt
-//         updatedAt
-//         academyTerm_id {
-//             id
-//             title_ar
-//             title_en
-//             status
-//             study_year
-//             current_year
-//             term_number
-//             min_study_hours
-//             max_study_hours
-//             faculty_department_id
-//         }
-//          material_id {
-//             id
-//             title_ar
-//             title_en
-//             status
-//             fullmark_degree
-//             success_degree
-//             material_hours
-//             createdAt
-//             updatedAt
-//         }
-//     }
-// }
-
-// `;
 
