55df17c77dce3c0281eba07b73fff9892364f890
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2025-11-16 12:32:13 +0200

    ayman 16/11/2025

--- src/graphql/registerationFormQueries.js
+++ src/graphql/registerationFormQueries.js
@@ -25,9 +25,9 @@ mutation CreateRegisterForm($input: RegisterFormInput!) {
 `;
 
 export const GET_REGISTERATION_FORM_BY_USER_ID=gql`
-query GetRegisterFormByUserId($user_id : ID!) {
+query GetRegisterFormByUserId($user_id:ID!) {
     getRegisterFormByUserId(user_id: $user_id) {
-     id
+        id
         first_name
         second_name
         third_name
@@ -89,9 +89,49 @@ query GetRegisterFormByUserId($user_id : ID!) {
             title_ar
             title_en
             status
-            faculty_id
             createdAt
             updatedAt
+        }
+        academyTerm_id {
+            id
+            title_ar
+            title_en
+            status
+            study_year
+            current_year
+            term_number
+            min_study_hours
+            max_study_hours
+            materials_array {
+                id
+                title_ar
+                title_en
+                status
+                fullmark_degree
+                success_degree
+                faculty_department_id
+                material_hours
+                createdAt
+                updatedAt
+            }
+            faculty_department_id {
+                id
+                title_ar
+                title_en
+                status
+                createdAt
+                updatedAt
+                faculty_id {
+                    id
+                    title_ar
+                    title_en
+                    status
+                    required_dep
+                    study_years_count
+                    createdAt
+                    updatedAt
+                }
+            }
         }
         country_id {
             id
@@ -108,32 +148,120 @@ query GetRegisterFormByUserId($user_id : ID!) {
             createdAt
             updatedAt
         }
-        academyTerm_id {
-            id
-            title_ar
-            title_en
-            status
-            study_year
-            current_year
-            term_number
-            min_study_hours
-            max_study_hours
-            faculty_department_id
-            materials_array{
-          id
-        title_ar
-        title_en
-        status
-        fullmark_degree
-        success_degree
-        faculty_department_id
-        material_hours
-        createdAt
-        updatedAt
-        }
-        }
-
-            }
+    }
 }
 
 `;
+// export const GET_REGISTERATION_FORM_BY_USER_ID=gql`
+// query GetRegisterFormByUserId($user_id : ID!) {
+//     getRegisterFormByUserId(user_id: $user_id) {
+//      id
+//         first_name
+//         second_name
+//         third_name
+//         fourth_name
+//         birthdate
+//         gender
+//         is_paid
+//         paid_document_file
+//         high_school_certificate_file
+//         address
+//         status
+//         mobile
+//         home_tel
+//         email
+//         is_inside_yemen
+//         transactions_id
+//         national_id_type
+//         national_id
+//         education_year
+//         study_place
+//         high_school_student_number
+//         general_grade
+//         gpa
+//         createdAt
+//         updatedAt
+//         user_id {
+//             id
+//             username
+//             fullname
+//             email
+//             mobile
+//             role
+//             status
+//             profile_image
+//             qid_number
+//             createdAt
+//             updatedAt
+//         }
+//         nationality_id {
+//             id
+//             name_ar
+//             name_en
+//             flag
+//             createdAt
+//             updatedAt
+//         }
+//         faculty_id {
+//             id
+//             title_ar
+//             title_en
+//             status
+//             required_dep
+//             study_years_count
+//             createdAt
+//             updatedAt
+//         }
+//         faculty_department_id {
+//             id
+//             title_ar
+//             title_en
+//             status
+//             faculty_id
+//             createdAt
+//             updatedAt
+//         }
+//         country_id {
+//             id
+//             name_ar
+//             name_en
+//             createdAt
+//             updatedAt
+//         }
+//         city_id {
+//             id
+//             name_ar
+//             name_en
+//             country_id
+//             createdAt
+//             updatedAt
+//         }
+//         academyTerm_id {
+//             id
+//             title_ar
+//             title_en
+//             status
+//             study_year
+//             current_year
+//             term_number
+//             min_study_hours
+//             max_study_hours
+//             faculty_department_id
+//             materials_array{
+//           id
+//         title_ar
+//         title_en
+//         status
+//         fullmark_degree
+//         success_degree
+//         faculty_department_id
+//         material_hours
+//         createdAt
+//         updatedAt
+//         }
+//         }
+
+//             }
+// }
+
+// `;
