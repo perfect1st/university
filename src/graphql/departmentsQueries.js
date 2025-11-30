578efa0543e9f8ff95560b47be7eb22c453f39a7
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2025-11-30 16:57:21 +0200

    ayman 30/11/2025

--- src/graphql/departmentsQueries.js
+++ src/graphql/departmentsQueries.js
@@ -31,3 +31,95 @@ export const getDepartmentByFatherId = gql`
     }
   }
 `;
+
+export const GET_WEBSITE_DEPARTMENTS_BY_ADMIN=gql`
+query WebsiteDepartments {
+    websiteDepartments {
+        id
+        title_ar
+        title_en
+        desc_ar
+        desc_en
+        image
+        status
+        createdAt
+        updatedAt
+        father_id {
+            id
+            title_ar
+            title_en
+            desc_ar
+            desc_en
+            image
+            status
+            createdAt
+            updatedAt
+            father_id {
+                id
+                title_ar
+                title_en
+                desc_ar
+                desc_en
+                image
+                status
+                createdAt
+                updatedAt
+            }
+        }
+    }
+}
+`;
+
+export const CREATE_WEBSITE_DEPARTMENT_BY_ADMIN=gql`
+mutation CreateWebsiteDepartment($input:CreateWebsiteDepartmentInput!) {
+    createWebsiteDepartment(input: $input) {
+        id
+        title_ar
+        title_en
+        desc_ar
+        desc_en
+        image
+        status
+          father_id {
+            id
+            title_ar
+            title_en
+            desc_ar
+            desc_en
+            image
+            status
+            createdAt
+            updatedAt
+        }
+        createdAt
+        updatedAt
+    }
+}
+`;
+
+export const UPDATE_WEBSITE_DEPARTMENT_BY_ID=gql`
+mutation UpdateWebsiteDepartment($id:ID!,$input:UpdateWebsiteDepartmentInput!) {
+    updateWebsiteDepartment(id: $id, input: $input) {
+        id
+        title_ar
+        title_en
+        desc_ar
+        desc_en
+        image
+        status
+        createdAt
+        updatedAt
+        father_id {
+            id
+            title_ar
+            title_en
+            desc_ar
+            desc_en
+            image
+            status
+            createdAt
+            updatedAt
+        }
+    }
+}
+`;
