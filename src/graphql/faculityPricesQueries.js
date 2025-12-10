92cc77053a87d61fd45d220d4b21b74cc86de1f9
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2025-12-10 17:38:07 +0200

    start 10/12/2025

--- src/graphql/faculityPricesQueries.js
+++ src/graphql/faculityPricesQueries.js
@@ -43,6 +43,56 @@ mutation CreateFacultyPrice($input:FacultyPriceInput!) {
 }
 `;
 
+export const GET_ALL_FACULITY_PRICES_FILTERED=gql`
+query FilteredPagedFacultyPrices(
+    $search: String
+    $faculty_id:ID
+    $faculty_department_id:ID
+    $level_year: Int!
+    $status: Boolean
+    $limit: Int!
+    $page: Int!
+    ) {
+    filteredPagedFacultyPrices(
+        search: $search
+        faculty_id: $faculty_id
+        faculty_department_id: $faculty_department_id
+        level_year: $level_year
+        status: $status
+        page: $page
+        limit: $limit
+    ) {
+        total
+        facultyPrices {
+            id
+            level_year
+            price_inside_yemen
+            price_outside_yemen
+            createdAt
+            updatedAt
+            faculty_id {
+                id
+                title_ar
+                title_en
+                status
+                required_dep
+                study_years_count
+                createdAt
+                updatedAt
+            }
+            faculty_department_id {
+                id
+                title_ar
+                title_en
+                status
+                createdAt
+                updatedAt
+            }
+        }
+    }
+}
+`;
+
 export const UPDATE_FACULITY_PRICE_BY_ID=gql`
 mutation UpdateFacultyPrice($id:ID!,$input:FacultyPriceInput!) {
     updateFacultyPrice(id: $id, input: $input) {
